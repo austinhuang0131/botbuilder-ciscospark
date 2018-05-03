fb8cd6e9e502c76323db49a458041420273471b9
Austin Huang <im@austinhuang.me>
2018-05-03 19:44:04 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,88 +2,114 @@ const Flint = require("node-flint");
 
 function Create(options) {
   // Option check
-  if (!options.name) throw "BotBuilder-CiscoSpark > Name argument (username@sparkbot.io) not defined.";
-  if (!options.token) throw "BotBuilder-CiscoSpark > Token argument not defined.";
-  if (!options.webhookUrl) throw "BotBuilder-CiscoSpark > Webhook URL argument not defined.";
-  if (!options.port) throw "BotBuilder-CiscoSpark > Webhook port argument not defined.";
+  if (!options.name)
+    throw "BotBuilder-CiscoSpark > Name argument (username@sparkbot.io) not defined.";
+  if (!options.token)
+    throw "BotBuilder-CiscoSpark > Token argument not defined.";
+  if (!options.webhookUrl)
+    throw "BotBuilder-CiscoSpark > Webhook URL argument not defined.";
+  if (!options.port)
+    throw "BotBuilder-CiscoSpark > Webhook port argument not defined.";
 
   // Define random stuff
-	var bot = new Flint({
-		token: options.token,
-		webhookUrl: options.webhookUrl,
-		port: options.port,
-		messageFormat: "markdown"
-	});
-	bot.start();
-  this.onEvent = (handler) => this.handler = handler;
-  this.startConversation = () => {if (options.debug) console.log("BotBuilder-CiscoSpark > startConversation", arguments)};
-  this.onInvoke = () => {if (options.debug) console.log("BotBuilder-CiscoSpark > onInvoke", arguments)};
-  
+  var bot = new Flint({
+    token: options.token,
+    webhookUrl: options.webhookUrl,
+    port: options.port,
+    messageFormat: "markdown"
+  });
+  bot.start();
+  this.onEvent = handler => (this.handler = handler);
+  this.startConversation = () => {
+    if (options.debug)
+      console.log("BotBuilder-CiscoSpark > startConversation", arguments);
+  };
+  this.onInvoke = () => {
+    if (options.debug)
+      console.log("BotBuilder-CiscoSpark > onInvoke", arguments);
+  };
+
   // Text-only Messages reception
-  bot.on("message", function (bot, message, id) {
+  bot.on("message", function(bot, message, id) {
     if (message.email === options.name || message.files) return;
-    if (options.debug) console.log("BotBuilder-CiscoSpark > New text message", message);
-	  var msg = {
-		timestamp: Date.parse(message.created),
-		source: "ciscospark",
-		entities: [],
-		text: message.text.replace(/^ /, ""),
-		address: {
-				bot: { name: options.name, id: bot.person.id },
-				user: { name: message.personEmail, id: message.personId },
-				channelId: "directline",
-				channelName: "ciscospark",
-				msg: message,
-				conversation: {
-					id: message.roomId,
-					isGroup: message.roomType === "group" ? true : false
-				}
-		}
-	};
-	this.handler([msg]);
-	if (options.debug) console.log("BotBuilder-CiscoSpark > Processed text message", msg);
+    if (options.debug)
+      console.log("BotBuilder-CiscoSpark > New text message", message);
+    var msg = {
+      timestamp: Date.parse(message.created),
+      source: "ciscospark",
+      entities: [],
+      text: message.text.replace(/^ /, ""),
+      address: {
+        bot: { name: options.name, id: bot.person.id },
+        user: { name: message.personEmail, id: message.personId },
+        channelId: "ciscospark",
+        channelName: "ciscospark",
+        msg: message,
+        conversation: {
+          id: message.roomId,
+          isGroup: message.roomType === "group" ? true : false
+        }
+      }
+    };
+    this.handler([msg]);
+    if (options.debug)
+      console.log("BotBuilder-CiscoSpark > Processed text message", msg);
   });
-  
+
   // Messages w/ att reception
-  bot.on("files", function (bot, message, id) {
+  bot.on("files", function(bot, message, id) {
     if (message.email === options.name) return;
-    if (options.debug) console.log("BotBuilder-CiscoSpark > New file message", message);
-		var msg = {
-			timestamp: Date.parse(message.created),
-			source: "ciscospark",
-			entities: [],
-			text: !message.text ? null : message.text.replace(/^ /, ""),
-			attachments: message.files.map(f => {return {content: f.binary, contentType: f.type}}),
-			address: {
-				bot: { name: options.name, id: bot.person.id },
-				user: { name: message.personEmail, id: message.personId },
-				channelId: "directline",
-				channelName: "ciscospark",
-				msg: message,
-				conversation: {
-					id: message.roomId,
-					isGroup: message.roomType === "group" ? true : false
-				}
-			}
-		};
-		this.handler([msg]);
-    if (options.debug) console.log("BotBuilder-CiscoSpark > Processed file message", msg);
+    if (options.debug)
+      console.log("BotBuilder-CiscoSpark > New file message", message);
+    var msg = {
+      timestamp: Date.parse(message.created),
+      source: "ciscospark",
+      entities: [],
+      text: !message.text ? null : message.text.replace(/^ /, ""),
+      attachments: message.files.map(f => {
+        return { content: f.binary, contentType: f.type };
+      }),
+      address: {
+        bot: { name: options.name, id: bot.person.id },
+        user: { name: message.personEmail, id: message.personId },
+        channelId: "ciscospark",
+        channelName: "ciscospark",
+        msg: message,
+        conversation: {
+          id: message.roomId,
+          isGroup: message.roomType === "group" ? true : false
+        }
+      }
+    };
+    this.handler([msg]);
+    if (options.debug)
+      console.log("BotBuilder-CiscoSpark > Processed file message", msg);
   });
-  
+
   // Message dispatching
   this.send = function(messages, cb) {
-		if (options.debug) console.log("BotBuilder-CiscoSpark > Sending messages... " + messages);
-		var body = [];
-		messages.map(msg => {
-			if (!msg.attachments) body.push({roomId: msg.address.conversation.id, text: msg.text});
-			else if (msg.attachments.length === 1) body.push({roomId: msg.address.conversation.id, text: msg.text, file: msg.attachments[0].contentUrl});
-			else console.error("BotBuilder-CiscoSpark > ERROR: You CANNOT send more than 1 attachment in a message.");
-		});
-		if (body.length !== 0) body.map(m => bot.say(m.roomId, m.text, m.file));
+    if (options.debug)
+      console.log("BotBuilder-CiscoSpark > Sending messages... " + messages);
+    var body = [];
+    messages.map(msg => {
+      if (!msg.attachments)
+        body.push({ roomId: msg.address.conversation.id, text: msg.text });
+      else if (msg.attachments.length === 1)
+        body.push({
+          roomId: msg.address.conversation.id,
+          text: msg.text,
+          file: msg.attachments[0].contentUrl
+        });
+      else
+        console.error(
+          "BotBuilder-CiscoSpark > ERROR: You CANNOT send more than 1 attachment in a message."
+        );
+    });
+    if (body.length !== 0) body.map(m => bot.say(m.roomId, m.text, m.file));
   };
-	
-	// Listener
-this.listen = require('node-flint/webhook')(bot);
+
+  // Listener
+  this.listen = require("node-flint/webhook")(bot);
   return this;
 }
 
