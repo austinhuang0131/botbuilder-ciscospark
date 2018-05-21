d3a8b7e4a6855607c76071d56c59092d14ac9c48
Austin Huang <im@austinhuang.me>
2018-05-21 16:10:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,26 +9,29 @@ var sparkConnector = (function() {
       throw "BotBuilder-CiscoSpark > Token argument not defined.";
     if (!options.port)
       throw "BotBuilder-CiscoSpark > Webhook port argument not defined.";
-    this.constructor.options = options;
+    this.options = options;
   }  
   // Define random stuff
   sparkConnector.prototype.onEvent = handler => this.handler = handler;
   sparkConnector.prototype.startConversation = () => {
-    if (this.options.debug)
+    var self = this;
+    if (self.options.debug)
       console.log("BotBuilder-CiscoSpark > startConversation", arguments);
   };
   sparkConnector.prototype.onInvoke = () => {
-    if (this.options.debug)
+    var self = this;
+    if (self.options.debug)
       console.log("BotBuilder-CiscoSpark > onInvoke", arguments);
   };
 
   // Listener
   sparkConnector.prototype.listen = (req, res) => {
-    if (this.options.debug) console.log("BotBuilder-CiscoSpark > Message received", req.body);
+    var self = this;
+    if (self.options.debug) console.log("BotBuilder-CiscoSpark > Message received", req.body);
     res.send("ok");
     let message = req.body;
-    if (message.data.personEmail !== this.options.name) snekfetch.get("https://api.ciscospark.com/v1/messages/"+message.data.id)
-    .set(`Authorization`, "Bearer " + this.options.token)
+    if (message.data.personEmail !== self.options.name) snekfetch.get("https://api.ciscospark.com/v1/messages/"+message.data.id)
+    .set(`Authorization`, "Bearer " + self.options.token)
     .then(r => {
       var msg = {
         timestamp: Date.parse(r.created),
@@ -37,11 +40,11 @@ var sparkConnector = (function() {
         text: !r.body.text ? null : r.body.text.replace(/^ /, ""),
         attachments: !r.body.files ? [] : r.body.files.map(f => {
           snekfetch.get(f)
-          .set(`Authorization`, "Bearer " + this.options.token)
+          .set(`Authorization`, "Bearer " + self.options.token)
           .then(a => {return {content: a.body, contentType: require("buffer-type")(a.body).type}});
         }),
         address: {
-          bot: { name: this.options.name, id: "placeholder" },
+          bot: { name: self.options.name, id: "placeholder" },
           user: { name: r.body.personEmail, id: r.body.personId },
           channelId: "cisco",
           channelName: "ciscospark",
@@ -52,19 +55,20 @@ var sparkConnector = (function() {
           }
         }
       };
-      this.handler([msg]);
-      if (this.options.debug)
+      self.handler([msg]);
+      if (self.options.debug)
         console.log("BotBuilder-CiscoSpark > Processed message", msg);
     });
   };
 
   sparkConnector.prototype.send = function(messages, cb) {
+    var self = this;
     if (messages.filter(m => m.source === "ciscospark").length !== messages.length)
       return Promise.reject(
         "BotBuilder-CiscoSpark > Ignoring messages for other platforms..." +
           JSON.stringify(messages)
       );
-    if (this.options.debug)
+    if (self.options.debug)
       console.log("BotBuilder-CiscoSpark > Preparing messages to go... " + messages);
     var body = [];
     messages.map(msg => {
@@ -81,14 +85,14 @@ var sparkConnector = (function() {
       }
       else body.push({ roomId: msg.address.conversation.id, markdown: msg.text });
     });
-    if (this.options.debug)
+    if (self.options.debug)
       console.log("BotBuilder-CiscoSpark > Messages ready to go... " + JSON.stringify(body));
     if (body.length !== 0) body.forEach(b => {
       snekfetch.post("https://api.ciscospark.com/v1/messages")
       .set(`Authorization`, "Bearer " + this.options.token)
       .set(`Content-Type`, "application/json")
       .send(b)
-      .then(r => {if (this.options.debug) console.log("BotBuilder-CiscoSpark > Here it goes... ", r.body);});
+      .then(r => {if (self.options.debug) console.log("BotBuilder-CiscoSpark > Here it goes... ", r.body);});
     });
   };
   return sparkConnector;
